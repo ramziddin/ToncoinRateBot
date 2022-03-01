c595baddc8918ebfd5da572363a961bb17c4d071
Ramziddin Makhmudov <ramziddin.makhmudov@gmail.com>
2022-03-01 16:02:15 +0500

    Remove weird duplicates

--- src/lib/t 2.ts
+++
@@ -1,9 +0,0 @@
-import i18n from "./i18n"
-import { Prisma } from "@prisma/client"
-
-export default function t(
-  phrase: string,
-  locale: Prisma.UserCreateInput["language"]
-) {
-  return i18n.__({ phrase, locale: locale?.toString() })
-}
